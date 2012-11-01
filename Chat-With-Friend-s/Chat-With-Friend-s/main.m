43a038b9def5324b87033cc7414578b09bac5dc1
Akhil Jagtap <mac@akhil.local>
2012-11-01 16:07:50 +0530

    Add Project Code

---
+++ Chat-With-Friend-s/Chat-With-Friend-s/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Chat-With-Friend-s
+//
+//  Created by praveen on 01/11/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
